ee0b47754012fc5f69b90dc00a54e13c781d07c2
Jon Jones <jon.jones@dc.gov>
2019-03-19 10:09:53 -0400

    edit button

--- src/components/myModules/Support/Support.js
+++ src/components/myModules/Support/Support.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import "./Support.css"
 import {SupportType} from '../../../SignworksJSON'
-import {faBinoculars} from '@fortawesome/free-solid-svg-icons';
+import {faBinoculars, faUserEdit} from '@fortawesome/free-solid-svg-icons';
 import {faGoogle} from '@fortawesome/free-brands-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+
 export default function Support(props) {
     //console.log('props', props)
     const SBT = new SupportType();
     let myImage = null;
     const getImage = () => {
         if (props.sel) {
-         
+
             if (props.sel.attributes.SUPPORTTYPE) {
                 myImage = window.location.origin + "/img/supports/" + props.sel.attributes.SUPPORTTYPE + ".png"
             } else {
@@ -25,29 +27,26 @@ export default function Support(props) {
     return (
 
         <div className="Support">
-          
+
             <img src={getImage()} className="SupportImage"/>
-            <p> Sign Type:
-                {props.sel
+            <p>
+                Sign Type: {props.sel
                     ? SBT.name(props.sel.attributes.SUPPORTTYPE, "SUPPORTTYPE")
                     : 'null '}
             </p>
             <p>
-              Sign Status:
-            {props.sel
+                Sign Status: {props.sel
                     ? SBT.name(props.sel.attributes.SUPPORTSTATUS, "SUPPORTSTATUS")
                     : 'null '}
             </p>
             <p>
-              <button ><FontAwesomeIcon
-            icon={faBinoculars}
-            title="StreetSmart"
-            /></button>
-              <button><FontAwesomeIcon
-            icon={faGoogle}
-            title="Needs Arrow"
-            /></button>
-              
+                <button ><FontAwesomeIcon icon={faBinoculars} title="StreetSmart"/></button>
+                <button><FontAwesomeIcon icon={faGoogle} title="Google Street View"/></button>
+            </p>
+            <p>
+                <button >
+                    <FontAwesomeIcon icon={faUserEdit} title="Google Street View"/>
+                </button>
             </p>
 
         </div>
