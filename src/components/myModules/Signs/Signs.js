7036b00ec13c1ec0ee6050a8a75c297ceed154fd
Jon Jones <jon.jones@dc.gov>
2019-03-19 13:38:26 -0400

    new components

---
+++ src/components/myModules/Signs/Signs.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Sign from './Sign';
+
+export default class Signs extends Component {
+
+
+
+
+  render() {
+    return (
+      <div>
+       <Sign></Sign>
+      </div>
+    )
+  }
+}
