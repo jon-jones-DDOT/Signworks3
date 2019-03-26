fb4c40ab95a504dba338b09490d5cfadbf2f411c
Jon Jones <jon.jones@dc.gov>
2019-03-26 15:06:24 -0400

    better band

--- src/components/myModules/Timebands/Timeband.js
+++ src/components/myModules/Timebands/Timeband.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function Timeband(props) {
+  let bob=  props.value.attributes.OBJECTID
   return (
-    <div>
-      {props.value.attributes.OBJECTID}
+    <div className= "TimebandRow">
+    <select><option>5AM</option></select>
+    <select><option>5AM</option></select>
+    <select><option>5AM</option></select>
+    <select><option>5AM</option></select>
+    <select><option>5AM</option></select>
+     
     </div>
   )
 }
--- src/components/myModules/Timebands/Timebands.js
+++ src/components/myModules/Timebands/Timebands.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Timeband from "./Timeband"
 
 export default function Timebands(props) {
-    
 
-  return (
-    <div>
-      
-      {props.bands.map((value, index) => (<Timeband key={`item-${index}`} index={index} value={value}/>))}
-    
-    </div>
-  )
+    const makeBands = () => {
+        return props.bands.map((value, index) => (<Timeband key={`item-${index}`} index={index} value={value}/>))
+    }
+
+    return (
+        <div>
+            Time Restrictions for this sign:
+            <br/> 
+            {props.bands.length > 0 ?makeBands():"No Restrictions"}
+
+        </div>
+    )
 }
